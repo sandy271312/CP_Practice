a96a432c75f5776d22970cb449f87dac36e24d7e
sandy271312 <gm271312@gmail.com>
2024-05-10 10:15:45 +0530

    No of leaf in abinary tree count function is added

---
+++ count_leaf_binarytree.cpp
@@ -0,0 +1,8 @@
+int count_leaf(Tree *root)
+{
+    if(!root)return 0;
+    if(!(root->left) && !(root->right))
+    return 1;
+    return count_leaf(root->left)+count_leaf(root->right);
+}
+/// This function will count the number the leaf in a binary tree
